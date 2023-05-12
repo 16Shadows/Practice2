3f2b211f6fe2ffdfaf43dfceddfe51c5cbdefb3c
Miraellax <catchit@ya.ru>
2023-05-12 20:22:04 +0500

    Added tables in DBContext

--- WebAPP/DMOrganizerDBContext.cs
+++ WebAPP/DMOrganizerDBContext.cs
@@ -15,5 +15,9 @@ namespace WebAPP
         public DbSet<Category> Categories { get; set; }
         public DbSet<Document> Documents { get; set; }
         public DbSet<Section> Sections { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<PageDMO> Pages { get; set; }
+        public DbSet<ContainerDMO> Containers { get; set;}
+        public DbSet<ObjectDMO> Objects { get; set;}
     }
 }
