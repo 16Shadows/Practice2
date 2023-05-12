84d01e6024dbaa74aa3a3988a80199bcfcc754bf
Miraellax <catchit@ya.ru>
2023-05-12 22:55:10 +0500

    Fixed relations object-container in db, updated db

--- DMOrganizerDomainModel/ObjectDMO.cs
+++ DMOrganizerDomainModel/ObjectDMO.cs
@@ -8,8 +8,8 @@ namespace DMOrganizerDomainModel
         public int Id { get; set; }
         public string LinkToObject { get; set; }
 
-        // required many-to-many: pages/containers
-        public virtual List<PageDMO> PageDMOs { get; set; } = new List<PageDMO>();
+        // required many-to-many: containers/objects
+        public virtual List<ContainerDMO> ContainerDMOs { get; set; } = new List<ContainerDMO>();
     }
     #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
