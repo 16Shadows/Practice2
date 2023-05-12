a802f1555f193df1878eeec9aa8d636da6cfec9a
16Shadows <egor.0.00@mail.ru>
2023-05-12 16:45:53 +0500

    Set up domain model

---
+++ DMOrganizerDomainModel/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DMOrganizerWebModel
+{
+    public class Category
+    {
+        public Category(int categoryId, string name, Category parent)
+        {
+            ID = categoryId;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Parent = parent ?? throw new ArgumentNullException(nameof(parent));
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Category Parent { get; set; }
+
+
+    }
+}
