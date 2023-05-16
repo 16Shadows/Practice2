936be09349dea17e0a6de9e723c9e04aa807bb02
16Shadows <egor.0.00@mail.ru>
2023-05-16 21:01:07 +0500

    Implemented creation, deletion an renaming of organizers.

---
+++ WebAPP/Controllers/IndexController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPP.Controllers
+{
+	[Route("")]
+	public class IndexController : Controller
+	{
+		[HttpGet("")]
+		public IActionResult Index()
+		{
+			return LocalRedirect("/Organizers");
+		}
+	}
+}
