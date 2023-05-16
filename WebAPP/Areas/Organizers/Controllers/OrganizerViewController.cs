8754e69f69594154a036120ed088abb466cff35f
16Shadows <egor.0.00@mail.ru>
2023-05-16 21:37:24 +0500

    Implemented "open" button for organizers properly redirecting to each organizer.

---
+++ WebAPP/Areas/Organizers/Controllers/OrganizerViewController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPP.Areas.Identity.Data;
+
+namespace WebAPP.Areas.Organizers.Controllers
+{
+	[Area("Organizers")]
+	[Route("Organizers/{id:int:required}")]
+	public class OrganizerViewController : Controller
+	{
+		private readonly UserManager<UserAccount> userManager;
+        private readonly WebAPPContext dbContext;
+
+        public OrganizerViewController(WebAPPContext context, UserManager<UserAccount> manager)
+        {
+            userManager = manager;
+            dbContext = context;
+        }
+
+		[HttpGet]
+		public async Task<IActionResult> Index(int id)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			bool any = dbContext
+					   .Organizers
+					   .Any(x => x.OwnerId == user.Id && x.Id == id);
+
+			if (!any)
+				return NotFound();
+
+			return View("Organizer");
+		}
+	}
+}
