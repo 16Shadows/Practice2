8377b8381ac9304e17068da395adbeab51fba135
16Shadows <egor.0.00@mail.ru>
2023-05-17 22:09:55 +0500

    Set up CategoryController.

---
+++ WebAPP/Areas/Organizers/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPP.Areas.Organizers.Controllers
+{
+	[Area("Organizers")]
+	[Route("Organizers/{organizerId:int:required}/category/{categoryId:int:required}")]
+	public class CategoryController : Controller
+	{
+		[HttpGet("")]
+		public IActionResult Root()
+		{
+			return View();
+		}
+	}
+}
