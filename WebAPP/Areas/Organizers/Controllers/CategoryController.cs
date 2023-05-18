63de3a0c4e9c36c71d3f046094e63d849a46c794
16Shadows <egor.0.00@mail.ru>
2023-05-18 20:30:01 +0500

    Implemented category controller and partial document controller.

--- WebAPP/Areas/Organizers/Controllers/CategoryController.cs
+++ WebAPP/Areas/Organizers/Controllers/CategoryController.cs
@@ -1,15 +1,180 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using System.Xml.XPath;
+using WebAPP.Areas.Identity.Data;
+using WebAPP.Areas.Organizers.Data;
+using WebAPP.Areas.Organizers.Models;
 
 namespace WebAPP.Areas.Organizers.Controllers
 {
+	[Authorize]
 	[Area("Organizers")]
 	[Route("Organizers/{organizerId:int:required}/category/{categoryId:int:required}")]
 	public class CategoryController : Controller
 	{
-		[HttpGet("")]
-		public IActionResult Root()
+		private readonly WebAPPContext dbContext;
+		private readonly UserManager<UserAccount> userManager;
+
+		public CategoryController(WebAPPContext dbContext, UserManager<UserAccount> userManager)
 		{
-			return View();
+			this.dbContext = dbContext;
+			this.userManager = userManager;
+		}
+
+		[HttpGet("")]
+		public async Task<ActionResult<CategoryBaseVM>> Root(int organizerId, int categoryId)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			if (!dbContext.HasOrganizer(user, categoryId))
+				return NotFound();
+
+			Category? target = dbContext
+							   .Categories
+							   .Where(x => x.Id == categoryId && x.OrganizerId == organizerId)
+							   .Include(x => x.Subcategories)
+							   .Include(x => x.Documents)
+							   .First();
+
+			if (target == null)
+				return NotFound();
+
+			return Ok(new CategoryBaseVM(target));
+		}
+
+		[HttpPost("createCategory/{name:required:minlength(1)}")]
+		public async Task<ActionResult<CategoryVM>> CreateCategory(int organizerId, int categoryId, string name)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			Organizer? organizer = dbContext.GetOrganizer(user, organizerId);
+
+			if (organizer == null) 
+				return NotFound();
+
+			Category? parent = dbContext.GetCategory(organizerId, categoryId);
+
+			if (parent == null)
+				return NotFound();
+
+			bool any = dbContext
+					   .Categories
+					   .Where(x => x.OrganizerId == organizerId && x.ParentId == categoryId && x.Name == name)
+					   .Include(x => x.Parent)
+					   .ToArray()
+					   .Any(x => x.Parent.Equals(parent));
+
+			if (any)
+				return Conflict();
+
+			Category category = new Category()
+			{
+				Name = name,
+				Parent = parent,
+				Organizer = organizer
+			};
+			dbContext.Categories.Add(category);
+
+			await dbContext.SaveChangesAsync();
+
+			return Ok(new CategoryVM(category));
+		}
+
+		[HttpPost("createDocument/{name:required:minlength(1)}")]
+		public async Task<ActionResult<DocumentVM>> CreateDocument(int organizerId, int categoryId, string name)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			Organizer? organizer = dbContext.GetOrganizer(user, organizerId);
+
+			if (organizer == null) 
+				return NotFound();
+
+			Category? parent = dbContext.GetCategory(organizerId, categoryId);
+
+			if (parent == null)
+				return NotFound();
+
+			bool any = dbContext
+					   .Documents
+					   .Where(x => x.OrganizerId == organizerId && x.CategoryId == categoryId && x.Title == name)
+					   .Include(x => x.Category)
+					   .ToArray()
+					   .Any(x => x.Category.Equals(parent));
+
+			if (any)
+				return Conflict();
+
+			Document document = new Document()
+			{
+				Title = name,
+				Category = parent,
+				Organizer = organizer
+			};
+			dbContext.Documents.Add(document);
+
+			await dbContext.SaveChangesAsync();
+
+			return Ok(new DocumentVM(document));
+		}
+
+		[HttpPost("rename/{name:required:minlength(1)}")]
+		public async Task<ActionResult<CategoryVM>> RenameCategory(int organizerId, int categoryId, string name)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			if (!dbContext.HasOrganizer(user, organizerId)) 
+				return NotFound();
+
+			Category? category = dbContext
+								 .Categories
+								 .Where(x => x.Id == categoryId && x.OrganizerId == organizerId)
+								 .Include(x => x.Parent)
+								 .ToArray()
+								 .FirstOrDefault();
+
+			if (category == null)
+				return NotFound();
+
+			bool any = dbContext
+					   .Categories
+					   .Where(x => x.OrganizerId == organizerId && x.ParentId == category.ParentId && x.Id != categoryId && x.Name == name)
+					   .Include(x => x.Parent)
+					   .ToArray()
+					   .Any(x => x.Parent.Equals(category.Parent));
+			
+			if (any)
+				return Conflict();
+
+			category.Name = name;
+			
+			await dbContext.SaveChangesAsync();
+
+			return Ok( new CategoryVM(category) );
+		}
+
+		[HttpDelete("delete")]
+		public async Task<ActionResult<CategoryVM>> DeleteCategory(int organizerId, int categoryId)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			if (!dbContext.HasOrganizer(user, organizerId)) 
+				return NotFound();
+
+			Category? category = dbContext.GetCategory(organizerId, categoryId);
+
+			if (category == null)
+				return NotFound();
+
+			CategoryVM vm = new CategoryVM(category);
+
+			dbContext.Categories.Remove(category);
+
+			await dbContext.SaveChangesAsync();
+
+			return Ok(vm);
 		}
 	}
 }
---
+++ WebAPP/Areas/Organizers/Controllers/DocumentController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using WebAPP.Areas.Identity.Data;
+using WebAPP.Areas.Organizers.Data;
+using WebAPP.Areas.Organizers.Models;
+
+namespace WebAPP.Areas.Organizers.Controllers
+{
+	[Authorize]
+	[Area("Organizers")]
+	[Route("Organizers/{organizerId:int:required}/document/{documentId:int:required}")]
+	public class DocumentController : Controller
+	{
+		private readonly WebAPPContext dbContext;
+		private readonly UserManager<UserAccount> userManager;
+
+		public DocumentController(WebAPPContext dbContext, UserManager<UserAccount> userManager)
+		{
+			this.dbContext = dbContext;
+			this.userManager = userManager;
+		}
+
+		[HttpGet("")]
+		public async Task<ActionResult> Root()
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			return Ok("Hello, world!");
+		}
+
+		[HttpPost("rename/{name:required:minlength(1)}")]
+		public async Task<ActionResult<CategoryVM>> RenameDocument(int organizerId, int documentId, string name)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			if (!dbContext.HasOrganizer(user, organizerId)) 
+				return NotFound();
+
+			Document? document = dbContext.GetDocument(organizerId, documentId);
+
+			if (document == null)
+				return NotFound();
+
+			bool any = dbContext
+					   .Documents
+					   .Where(x => x.OrganizerId == organizerId && x.CategoryId == document.CategoryId && x.Id != documentId && x.Title == name)
+					   .Include(x => x.Category)
+					   .ToArray()
+					   .Any(x => x.Category.Equals(document.Category));
+			
+			if (any)
+				return Conflict();
+
+			document.Title = name;
+
+			await dbContext.SaveChangesAsync();
+			
+			return Ok( new DocumentVM(document) );
+		}
+
+		[HttpDelete("delete")]
+		public async Task<ActionResult<CategoryVM>> DeleteDocument(int organizerId, int categoryId)
+		{
+			var user = await userManager.GetUserAsync(User);
+
+			if (!dbContext.HasOrganizer(user, organizerId)) 
+				return NotFound();
+
+			Document? document = dbContext.GetDocument(organizerId, categoryId);
+
+			if (document == null)
+				return NotFound();
+
+			DocumentVM vm = new DocumentVM(document);
+
+			dbContext.Documents.Remove(document);
+
+			await dbContext.SaveChangesAsync();
+
+			return Ok(vm);
+		}
+	}
+}
