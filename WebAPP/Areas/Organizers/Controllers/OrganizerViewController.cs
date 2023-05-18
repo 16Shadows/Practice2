9642e72dba8bc2a7f78dc4d535345eec024c4b2a
16Shadows <egor.0.00@mail.ru>
2023-05-18 16:24:49 +0500

    Extracted VMs from OrganizerViewController into separate classes.

--- WebAPP/Areas/Organizers/Controllers/OrganizerViewController.cs
+++ WebAPP/Areas/Organizers/Controllers/OrganizerViewController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
 using WebAPP.Areas.Identity.Data;
 using WebAPP.Areas.Organizers.Data;
+using WebAPP.Areas.Organizers.Models;
 
 namespace WebAPP.Areas.Organizers.Controllers
 {
@@ -9,42 +11,6 @@ namespace WebAPP.Areas.Organizers.Controllers
 	[Route("Organizers/{organizerId:int:required}")]
 	public class OrganizerViewController : Controller
 	{
-		public class ItemBaseVM
-		{
-			protected ItemBaseVM(string type, string name, int id)
-			{
-				Type = type ?? throw new ArgumentNullException(nameof(type));
-				Name = name ?? throw new ArgumentNullException(nameof(name));
-				Id = id;
-			}
-
-			public string Type { get; }
-			public string Name { get; }
-			public int Id { get; }
-		}
-
-		public class CategoryVM : ItemBaseVM
-		{
-			public CategoryVM(Category category) : base(nameof(Category), category.Name, category.Id) { }
-		}
-
-		public class DocumentVM : ItemBaseVM
-		{
-			public DocumentVM(Document document) : base(nameof(Document), document.Title, document.Id) { }
-		}
-
-		public class CategoryBaseVM
-		{
-			public CategoryBaseVM(CategoryBase category)
-			{
-				Subcategories = category.Subcategories.Select(x => new CategoryVM(x)).ToList();
-				Documents = category.Documents.Select(x => new DocumentVM(x)).ToList();
-			}
-
-			public List<CategoryVM> Subcategories { get; }
-			public List<DocumentVM> Documents { get; }
-		}
-
 		private readonly UserManager<UserAccount> userManager;
         private readonly WebAPPContext dbContext;
 
@@ -79,31 +45,15 @@ namespace WebAPP.Areas.Organizers.Controllers
 
 			Organizer? target = dbContext
 								.Organizers
-								.First(x => x.OwnerId == user.Id && x.Id == organizerId);
+								.Where(x => x.OwnerId == user.Id && x.Id == organizerId)
+								.Include(x => x.Subcategories)
+								.Include(x => x.Documents)
+								.First();
 
 			if (target == null)
 				return NotFound();
 
 			return Ok ( new CategoryBaseVM(target) );
-		}
-
-		[HttpGet("category/{categoryId:int:required}")]
-		public async Task<ActionResult<CategoryBaseVM>> Category(int organizerId, int categoryId)
-		{
-			var user = await userManager.GetUserAsync(User);
-
-			bool any = dbContext
-					   .Organizers
-					   .Any(x => x.OwnerId == user.Id && x.Id == organizerId);
-
-			if (!any)
-				return NotFound();
-
-			Category? category = dbContext
-								.Categories
-								.First(x => x.Id == categoryId && x.OrganizerId == organizerId);
-
-			return Ok ( new CategoryBaseVM(category) );
 		}
 
 		[HttpPost("category/create/{name:minlength(1):required}")]
---
+++ WebAPP/Areas/Organizers/Models/ItemBaseVM.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAPP.Areas.Organizers.Models
+{
+	public class ItemBaseVM
+	{
+		protected ItemBaseVM(string type, string name, int id)
+		{
+			Type = type ?? throw new ArgumentNullException(nameof(type));
+			Name = name ?? throw new ArgumentNullException(nameof(name));
+			Id = id;
+		}
+
+		public string Type { get; }
+		public string Name { get; }
+		public int Id { get; }
+	}
+}
