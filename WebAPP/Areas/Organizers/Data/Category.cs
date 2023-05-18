9809b9a17712d137e00d618b246c07219a82426c
16Shadows <egor.0.00@mail.ru>
2023-05-18 20:25:36 +0500

    Implemented equality comparison for category and organizer to allow proper comparison of parents of items.

--- WebAPP/Areas/Organizers/Data/Category.cs
+++ WebAPP/Areas/Organizers/Data/Category.cs
@@ -2,6 +2,7 @@
 
 namespace WebAPP.Areas.Organizers.Data;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
 [Index(nameof(Name), nameof(ParentId), IsUnique = true)]
 public class Category : CategoryBase
 {
@@ -9,5 +10,11 @@ public class Category : CategoryBase
     public CategoryBase Parent { get; set; }
     public int OrganizerId { get; set; }
     public Organizer Organizer { get; set; }
+
+	public override bool Equals(object? obj)
+	{
+		return obj is Category other && other.Id == Id;
+	}
 }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
